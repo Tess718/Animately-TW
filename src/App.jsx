30ca1f74a3176893d4e3f76d0a1f3fa8f5588c66
Tess718 <111273602+Tess718@users.noreply.github.com>
2024-04-24 12:47:30 +0000

    description

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import Footer from "./Components/Footer";
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Helmet } from "react-helmet";
 
 export default function App() {
   AOS.init();
   return (
     <>
+    <Helmet>
+      <meta name="description" content="Animately: Your destination for creating stunning animations effortlessly." />
+    </Helmet>
     <Navbar/>
     <Hero />
     <Lessons />
